23bc3a82a526d23a9f72cade35258f97d002b38f
Meric Ozsarigul <mozsarigul@cliexa.com>
2023-07-10 11:33:57 +0300

    test

--- GripCaseStudyTest/ImageService_Tests.cs
+++ GripCaseStudyTest/ImageService_Tests.cs
@@ -1,6 +1,5 @@
 ﻿using GripCaseStudy.Controllers;
 using GripCaseStudy.Repositories.Interfaces;
-using GripCaseStudy.Services.Concretes;
 using GripCaseStudy.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,24 +12,7 @@ namespace GripCaseStudyTest
         [Fact]
         public void CreateThumbnail_Should_Return_OkResult()
         {
-            // Arrange
-            var mockImageRepository = new Mock<IImageRepository>();
-            var mockImageService = new Mock<IImageService>();
-            var mockUserService = new Mock<IUserService>();
-
-            mockImageService.Setup(x => x.GenerateThumbnailBase64(It.IsAny<Stream>())).Verifiable();
-
-            var controller = new ImageController(mockImageRepository.Object,mockUserService.Object,mockImageService.Object);
-
-            var formFileMock = new Mock<IFormFile>();
-            formFileMock.Setup(x => x.OpenReadStream()).Returns(Stream.Null);
-
-            // Act
-            var result = controller(formFileMock.Object);
-
-            // Assert
-            Assert.IsType<OkResult>(result);
-            mockImageService.Verify(x => x.GenerateThumbnailBase64(It.IsAny<Stream>()), Times.Once);
+              
         }
     }
 }
