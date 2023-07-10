0e47b0678325e1ab6a4bb44e4fd708dd5a053adb
Meric Ozsarigul <mozsarigul@cliexa.com>
2023-07-10 11:37:35 +0300

    without test

---
+++ GripCaseStudyTest/TokenControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using GripCaseStudy.Controllers;
+using GripCaseStudy.Repositories.Concretes;
+using GripCaseStudy.Repositories.Interfaces;
+using GripCaseStudy.Services.Concretes;
+using GripCaseStudy.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GripCaseStudyTest
+{
+    public class TokenControllerTest
+    {
+        private readonly TokenController _controller;
+        private readonly IUserService _service;
+        private readonly IUserRepository _repo;
+
+        public TokenControllerTest()
+        {
+            _repo = new UserRepository();
+            _service = new UserService();
+            _controller = new ShoppingCartController(_service);
+        }
+        [Fact]
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+            // Act
+            var okResult = _controller.Get();
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult as OkObjectResult);
+        }
+        [Fact]
+        public void Get_WhenCalled_ReturnsAllItems()
+        {
+            // Act
+            var okResult = _controller.Get() as OkObjectResult;
+            // Assert
+            var items = Assert.IsType<List<ShoppingItem>>(okResult.Value);
+            Assert.Equal(3, items.Count);
+        }
+    }
+}
+}
